8c82e9c6bc918fef7c9db8f6dc55e74010529601
pleroux <p.leroux@gmx.com>
2019-12-22 11:52:56 +0100

    setup cabal on ubuntu

--- src/Goal.hs
+++ src/Goal.hs
@@ -7,7 +7,7 @@ spiral n =
   replace $ grade . scanl1 (+) . concat $ zipWith replicate (counts n) (values n)
   where grade xs = map snd . sort $ zip xs [1..]
         values n = cycle [1,n,-1,-n]
-        counts n = (n:) . concatMap ((<*>) (:) return) $ [n-1,n-2..1]
+        counts n = (n:) . concatMap (replicate 2) $ [n-1,n-2..1]
         len = n * n
         replace [] = []
         replace (x:xs)
