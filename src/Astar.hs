ccf18428af236d4c74b44b59b5a4bcbdf1a2ea5f
pleroux <p.leroux@gmx.com>
2019-12-22 22:20:58 +0100

    debug a star

--- src/Astar.hs
+++ src/Astar.hs
@@ -3,35 +3,40 @@ module Astar where
 import qualified Data.PQueue.Prio.Min as PQ
 import qualified Data.HashMap.Strict as Map
 import Heurestic(manhatan)
+import Move(move4Ways)
+import Data.Maybe(fromMaybe)
 
-run :: [Int] -> Maybe [Int]
-run startPuzzle = 
-  astar
-    (PQ.singleton (manhatan startPuzzle) startPuzzle) --opened
-    (Map.empty) --closed
-    (Map.singleton startPuzzle 0) --gscore
-    (Map.singleton startPuzzle $ manhatan startPuzzle) --fscore
+heurestic :: Int -> [Int] -> Int
+heurestic n puzzle = manhatan n puzzle
 
-astar :: MinPQueue Int [Int] -> HashMap [Int] [Int] -> HashMap [Int] Int -> HashMap [Int] Int-> Maybe [Int]
-astar opened closed gscore fscore
+run :: Int -> [Int] -> Maybe [Int]
+run n startPuzzle = 
+  let 
+    opened = PQ.singleton h startPuzzle
+    closed = Map.empty
+    gscore = Map.singleton startPuzzle 0
+    fscore = Map.singleton startPuzzle h
+    h = heurestic n startPuzzle
+  in astar n opened closed gscore fscore
+
+astar :: Int -> PQ.MinPQueue Int [Int] -> Map.HashMap [Int] [Int] -> Map.HashMap [Int] Int -> Map.HashMap [Int] Int-> Maybe [Int]
+astar n opened closed gscore fscore
   | PQ.null opened = Nothing
-  | manhatan list == 0 = Just(list)
-  | otherwise = astar newOpened newClosed newGscore newFscore
+  | heurestic n puzzle == 0 = Just(puzzle)
+  | otherwise = astar n newOpened newClosed newGscore newFscore
   where current = PQ.findMin opened
-        list = snd current
+        puzzle = snd current
         openedDeleteMin = PQ.deleteMin opened
-        filteredNeighbor = filter (\x -> tentativeGscore < getMap x gscore) $ move4ways current
+        filteredNeighbor = filter (\x -> tentativeGscore < getMap x gscore) $ move4Ways puzzle
         filteredNeighborNotInOpened = filter
                                         (\x ->
-                                          length (PQ.filterWithKey (\k a -> x == k) openedDeleteMin) >= 0)
+                                          PQ.size (PQ.filter (\a -> x == a) openedDeleteMin) == 0)
                                         filteredNeighbor
-        newOpened = fold (\x -> PQ.insert x openedDeleteMin) filteredNeighborNotInOpened
-        newClosed = fold (\x -> PQ.insert x closed) filteredNeighbor
-        newGscore = fold (\x -> PQ.insert x gscore) filteredNeighbor
-        newFscore = fold (\x -> PQ.insert x fscore) filteredNeighbor
-        tentativeGscore = (+1) Map.lookup current gscore
+        newOpened = foldr (\x -> PQ.insert (heurestic n x) x) openedDeleteMin filteredNeighborNotInOpened
+        newClosed = foldr (\x -> Map.insert x puzzle) closed filteredNeighbor
+        newGscore = foldr (\x -> Map.insert x tentativeGscore) gscore filteredNeighbor
+        newFscore = foldr (\x -> Map.insert x (tentativeGscore + heurestic n x)) fscore filteredNeighbor
+        tentativeGscore = (+1) $ getMap puzzle gscore
 
-getMap k hashMap =
-  | value == Nothing = maxBound :: Int
-  | otherwise = value
-  where value = Map.lookup k hashMap
+getMap :: [Int] -> Map.HashMap [Int] Int -> Int
+getMap k hashMap = fromMaybe (maxBound :: Int) $ Map.lookup k hashMap
